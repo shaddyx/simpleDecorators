96360c1f0b3b50bb0c5776657824eb48a7551f2b
Anatolii Yakushko <ayakushko@intropro.com>
2016-09-02 18:01:43 +0300

    [-] setup.py fixed

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
   name = 'simpleDecorators',
-  packages = ['simpledecorators'], # this must be the same as the name above
+  packages = ['simpledecorators'],
   version = '0.1',
   description = 'a collection of simple decorators',
   author = 'Anatolii Yakushko',
   author_email = 'shaddyx@gmail.com',
-  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+  url = 'https://github.com/shaddyx/simpleDecorators', # use the URL to the github repo
+  download_url = 'https://github.com/shaddyx/simpleDecorators/tarball/0.1',
   keywords = ['testing', 'logging', 'example'], # arbitrary keywords
   classifiers = [],
 )
