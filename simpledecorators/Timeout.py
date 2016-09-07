2366b9c695808517d2a8d06b12547ec19d3882e2
Anatolii Yakushko <ayakushko@intropro.com>
2016-09-07 16:21:59 +0300

    [+] timeout added

---
+++ simpledecorators/Timeout.py
@@ -0,0 +1,30 @@
+import time
+import signal
+
+class TimeoutError(Exception):
+    def __init__(self, value = "Timed Out"):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+def Timeout(seconds_before_timeout):
+    def decorate(f):
+        def handler(signum, frame):
+            raise TimeoutError()
+        def new_f(*args, **kwargs):
+            old = signal.signal(signal.SIGALRM, handler)
+            old_time_left = signal.alarm(seconds_before_timeout)
+            if 0 < old_time_left < second_before_timeout: # never lengthen existing timer
+                signal.alarm(old_time_left)
+            start_time = time.time()
+            try:
+                result = f(*args, **kwargs)
+            finally:
+                if old_time_left > 0: # deduct f's run time from the saved timer
+                    old_time_left -= time.time() - start_time
+                signal.signal(signal.SIGALRM, old)
+                signal.alarm(old_time_left)
+            return result
+        new_f.func_name = f.func_name
+        return new_f
+    return decorate
