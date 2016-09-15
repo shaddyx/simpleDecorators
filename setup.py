84806c5d498b44b8aed133cee04883f79c18b5ea
Anatolii Yakushko <ayakushko@intropro.com>
2016-09-15 17:49:01 +0300

    [+] version increased

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'simpleDecorators',
   packages = ['simpledecorators'],
-  version = '0.6',
+  version = '0.7',
   description = 'a collection of simple decorators',
   author = 'Anatolii Yakushko',
   author_email = 'shaddyx@gmail.com',
